3e93320d3b27d68c614a77480e48d9509f3f48cc
Anurag Dhote <anuragdhote392@gmail.com>
2023-10-24 22:58:42 +0530

    feat - complete image caption frontend

--- src/app/image-prompt/page.tsx
+++ src/app/image-prompt/page.tsx
@@ -8,55 +8,59 @@ import { Loader2 } from "lucide-react"
 type Props = {}
 
 const ImagePrompt = (props: Props) => {
+    const scrollRef = useRef<HTMLDivElement>(null);
     const selectedFile = useRef<HTMLInputElement>(null);
     const [loading, setLoading] = useState(false);
     const [selectedImage, setSelectedImage] = useState("");
+    const [description, setDescription] = useState("");
 
-    const handleSubmit = async () => {
-        setLoading(true);
+    const handleGenerateCaption = async () => {
         try {
-            console.log(process.env.NEXT_PUBLIC_SERVER_URL)
-            if (!selectedImage) return;
-
+            setLoading(true);
+            const imageData: any = selectedFile.current?.files?.[0];
+            const formData = new FormData();
+            formData.append('image', imageData);
             const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/image/image-caption`;
-            // console.log({selectedImage, url})
-            console.log(selectedImage)
-
             const response = await fetch(url, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ image: selectedImage, file: selectedFile.current?.files?.[0] })
+                body: formData
             })
-            const data = await response.json();
-            console.log(data);
+            const { success, message, data } = await response.json();
+            if (!success) {
+                throw new Error(message);
+            }
+            setDescription(data);
+            scrollRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
         } catch (error: any) {
-            console.log(error.response?.data);
+            console.error(error);
+            alert(error.message)
+        } finally {
+            setLoading(false);
         }
-        setLoading(false);
+    }
+
+    const onFileInputChange = async ({ target }: any) => {
+        if (target.files) {
+            const imageData = target.files[0];
+            const fileReader = new FileReader()
+            fileReader.readAsDataURL(imageData);
+            fileReader.onload = () => {
+                setSelectedImage(fileReader.result as string);
+            }
+        }
     }
 
     return (
-        <div className="max-w-4xl mx-auto px-20">
-            <h1 className='text-center text-3xl my-5'>Generate Captions for Images!</h1>
+        <div className="max-w-4xl mx-auto px-20" ref={scrollRef}>
+            <h1 className='text-center text-3xl my-5'>Generate Description for Images!</h1>
             <label htmlFor="">Browse Image</label>
             <form className='flex space-x-4' encType='multipart/form-data'>
                 <Input
                     ref={selectedFile}
                     id="image"
                     type='file'
-                    onChange={({ target }) => {
-                        if (target.files) {
-                            const file = target.files[0];
-
-                            const fileReader = new FileReader()
-                            fileReader.readAsDataURL(file);
-                            fileReader.onload = () => {
-                                setSelectedImage(fileReader.result as string);
-                            }
-                        }
-                    }}
+                    disabled={loading}
+                    onChange={onFileInputChange}
                 />
                 {selectedImage.length > 0 && (<Button
                     variant={"destructive"}
@@ -64,8 +68,10 @@ const ImagePrompt = (props: Props) => {
                         if (selectedFile.current) {
                             selectedFile.current.value = "";
                             setSelectedImage("");
+                            setDescription("");
                         }
                     }}
+                    disabled={loading}
                 >{"X"}</Button>)}
             </form>
             {selectedImage.length > 0 &&
@@ -77,24 +83,17 @@ const ImagePrompt = (props: Props) => {
                     )}
                 </div>}
 
-            {selectedImage.length === 0 && (
-                <Button disabled className='max-content my-5 block' onClick={handleSubmit}>
-                    {"Generate Caption"}
-                </Button>
-            )}
+            <Button disabled={selectedImage.length === 0 || loading} className='max-content my-5' onClick={handleGenerateCaption}>
+                {loading && (<Loader2 className="mr-2 h-4 w-4 animate-spin" />)}
+                {loading ? ("Please Wait") : ("Generate Caption")}
+            </Button>
 
-            {loading && (
-                <Button className='max-content my-5 block' disabled>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    {"Please wait"}
-                </Button>
+            {description.length > 0 && (
+                <div className='mt-4 mb-8'>
+                    <h1 className='text-2xl font-bold'>Generated Description:</h1>
+                    <p className='capitalize'>{description}</p>
+                </div>
             )}
-
-            {(selectedImage.length > 0 && !loading) && (
-                <Button className='max-content my-5 block' onClick={handleSubmit}>
-                    {`Generate Caption`}
-                </Button>
-            )} 
         </div>
     )
 }
